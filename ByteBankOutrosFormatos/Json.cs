8d1b4f3f2cba7b11b6e5a060e0f0b53690dc6ec9
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-02 14:18:22 -0300

    Biblioteca para gerar xml e json

---
+++ ByteBankOutrosFormatos/Json.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ByteBankOutrosFormatos
+{
+    public class Json<T>
+    {
+        public static void CriaJSON(Array list)
+        {
+            List<Object> lista = new List<Object>();
+            if (list.Length <= 0)
+            {
+                Console.WriteLine("... Não existe dados para exportação...");
+                Console.ReadKey();
+            }
+            else
+            {
+                string json = JsonConvert.SerializeObject(lista, Formatting.Indented);
+                try
+                {
+                    FileStream fs = new FileStream(@"C:\Users\AmandaMarquesPardinh\Desktop\Performa\repositorios\CSharp---Criando-e-Distribuindo-Bibliotecas\tmp\contas2.json",
+                        FileMode.Create);
+                    using (StreamWriter streamwriter = new StreamWriter(fs))
+                    {
+                        streamwriter.WriteLine(json);
+                    }
+                    Console.WriteLine(@"Arquivo salvo em c:\tmp\contas2.json");
+                    Console.ReadKey();
+                }
+                catch (Exception excecao)
+                {
+                    Console.WriteLine("Ocorreu um erro ao exportar as contas: " + excecao.Message);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ ByteBankOutrosFormatos/Xml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ByteBankOutrosFormatos
+{
+    public class Xml<T>
+    {
+        public static void CriaXML(Array list)
+        {
+            List<Object> lista = new List<Object>();
+            if (list.Length <= 0)
+            {
+                Console.WriteLine("... Não existe dados para exportação...");
+                Console.ReadKey();
+            }
+            else
+            {
+                try
+                {
+                    FileStream fileStreamXml = new FileStream(@"C:\Users\AmandaMarquesPardinh\Desktop\Performa\repositorios\CSharp---Criando-e-Distribuindo-Bibliotecas\tmp\contas3.xml", FileMode.Create);
+                    using (StreamWriter streamwriter = new StreamWriter(fileStreamXml))
+                    {
+                        XmlSerializer serializador = new XmlSerializer(typeof(List<Object>));
+
+                        serializador.Serialize(streamwriter, lista);
+                    }
+                    Console.WriteLine(@"Arquivo salvo em c:\tmp\contas3.xml");
+                    Console.ReadKey();
+                }
+                catch (Exception excecao)
+                {
+                    Console.WriteLine("Ocorreu um erro ao exportar as contas: " + excecao.Message);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
+
